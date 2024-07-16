b3c4d405222271ceb45cb621992d5ad6fe35e008
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-16 10:49:10 +0300

    initial commit

---
+++ module01/exercise01.py
@@ -0,0 +1,14 @@
+"""
+Abstraction/Modeling
+Object-Oriented Analysis and Design
+Requirement Model -> Analysis Model -> Design Model -> Implementation -> Software
+Architectural Design Model
+Solution Model
+
+OO -> i. Class and Object :-> Encapsulation & Information Hiding Principle
+         attribute/data/state/property
+         behaviour/method
+         Concept/Entity -> Class -> Object
+     ii. Inheritance      :-> Reusability
+    iii. Polymorphism     :-> Agility
+"""
---
+++ module01/exercise02.py
@@ -0,0 +1,35 @@
+class vehicle:
+    """
+        members:
+        1. attributes         -> licence_plate, capacity, load
+        2. (business) methods -> load, unload, constructor (__init__) -> create object
+    """
+
+    def __init__(self, licence_plate: str, capacity: float = 1_000) -> None:
+        self.licence_plate = licence_plate
+        self.capacity = capacity
+        self.current_load = 0
+
+    def load(self, weight: float) -> float:
+        """
+        business method: loads some weight to the vehicle
+        :param weight: weight in kg
+        :return: returns current weight in kg
+        """
+        # validation
+        if weight <= 0:
+            raise ValueError(f"weight ({weight}) must be a positive number.")
+        # business rule
+        if self.current_load + weight > self.capacity:
+            raise ValueError(f"Current load ({self.current_load}) exceeds capacity ({weight}).")
+        self.current_load += weight
+        return self.current_load
+
+    def unload(self, weight: float) -> float:
+        # validation
+        if weight <= 0:
+            raise ValueError(f"weight ({weight}) must be a positive number.")
+        if weight > self.current_load:
+            raise ValueError(f"weight ({weight}) should not exceed current load ({self.current_load}).")
+        self.current_load -= weight
+        return self.current_load
