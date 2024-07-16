94578ea456e7c387d6099d41a1a296b5043ce385
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-16 11:53:31 +0300

    Update exercise02.py

--- module01/exercise02.py
+++ module01/exercise02.py
@@ -9,11 +9,35 @@ class Color(Enum):
     BLACK = 5
 
 
+class OverCapacityError(Exception):
+    """
+    Business Exception
+    OverCapacityError is an Exception
+    OverCapacityError: subclass/derived class
+    Exception: super class/base class
+    Raised when an over-capacity error is encountered.
+    """
+
+    def __init__(self, message: str, over_weight: float):
+        super().__init__()
+        self.message = message
+        self.__over_weight = over_weight
+
+    @property
+    def over_weight(self):
+        return self.__over_weight
+
+    def __str__(self) -> str:
+        return f'OverCapacityError [{self.message}]: {self.over_weight}'
+
+
 class vehicle:
     """
         members:
         1. attributes         -> licence_plate, capacity, load
         2. (business) methods -> load, unload, constructor (__init__) -> create object
+                                 getter/setter
+                                 __str__: def __str__ object -> str
     """
 
     def __init__(self, licence_plate: str, capacity: float = 1_000, color: Color = Color.RED) -> None:
@@ -53,7 +77,9 @@ class vehicle:
             raise ValueError(f"weight ({weight}) must be a positive number.")
         # business rule
         if self.__current_load + weight > self.__capacity:
-            raise ValueError(f"Current load ({self.__current_load}) exceeds capacity ({self.__capacity}).")
+            over_weight = self.__current_load + weight - self.__capacity
+            message = f"Current load ({self.__current_load}) exceeds capacity ({self.__capacity})."
+            raise OverCapacityError(message=message, over_weight=over_weight)
         self.__current_load += weight
         return self.__current_load
 
@@ -66,17 +92,25 @@ class vehicle:
         self.__current_load -= weight
         return self.__current_load
 
+    def __str__(self) -> str:
+        return f"vehicle [license_plate: {self.licence_plate}, capacity: {self.capacity}, current_load: {self.current_load}, color: {self.color}]"
 
-vehicle1 = vehicle(licence_plate="34abc42", capacity=5_000)
-vehicle2 = vehicle("06def49", 2_500)
-vehicle3 = vehicle(capacity=8_000, licence_plate="07mn108")
-print(f"vehicle1's current load: {vehicle1.current_load}")
-vehicle1.load(weight=500)
-print(f"vehicle1's current load: {vehicle1.current_load}")
-vehicle1.load(weight=1_500)
-print(f"vehicle1's current load: {vehicle1.current_load}")
-vehicle1.load(weight=2_000)
-print(f"vehicle1's current load: {vehicle1.current_load}")
-vehicle1.color = Color.GREEN
-vehicle1.load(weight=1_500)
-print(f"vehicle1's current load: {vehicle1.current_load}")
+
+try:
+    vehicle1 = vehicle(licence_plate="34abc42", capacity=5_000)
+    vehicle2 = vehicle("06def49", 2_500)
+    vehicle3 = vehicle(capacity=8_000, licence_plate="07mn108")
+    print(f"vehicle1's current load: {vehicle1.current_load}")
+    vehicle1.load(weight=500)
+    print(f"vehicle1's current load: {vehicle1.current_load}")
+    vehicle1.load(weight=1_500)
+    print(f"vehicle1's current load: {vehicle1.current_load}")
+    vehicle1.load(weight=2_000)
+    print(f"vehicle1's current load: {vehicle1.current_load}")
+    vehicle1.color = Color.GREEN
+    vehicle1.load(weight=1_500)
+    print(f"vehicle1's current load: {vehicle1.current_load}")
+    print(vehicle1)  # F7BE<__main__.vehicle object at 0x0000020876B0>
+    print(str(vehicle1))
+except OverCapacityError as oce:
+    print(oce)
