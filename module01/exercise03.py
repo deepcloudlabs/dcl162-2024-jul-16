c7b3b6196a37ed36bd328514dab37f24ed0b949b
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-16 12:22:47 +0300

    Update exercise03.py

--- module01/exercise03.py
+++ module01/exercise03.py
@@ -1,3 +1,6 @@
+import math
+
+
 class square:
     def __init__(self, x: float, y: float, edge: float):
         self.__x = x
@@ -39,10 +42,45 @@ class cube(square):  # cube is a square
         return 6.0 * super().area()  # delegate to square's area
         # return 6.0 * super().edge * super().edge
 
+    def volume(self) -> float:
+        return super().edge ** 3
+
+
+class circle(square):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    @property
+    def radius(self) -> float:
+        return super().edge
+
+    def area(self) -> float:
+        return math.pi * self.radius * self.radius
+
+
+class sphere(circle):
+    def __init__(self, x, y, z, radius):
+        super().__init__(x, y, radius)
+        self.__z = z
+
+    def area(self) -> float:
+        return 4.0 * super().area()  # delegate to circle's area
+
+    def volume(self) -> float:
+        return (4.0 * math.pi * super().area()) / 3.0
+
 
 sq1 = square(1.0, 1.0, 5.0)
+c1 = circle(1.0, 1.0, 5.0)
 cube1 = cube(2.0, 3.0, 4.0, 5.0)
+sp1 = sphere(2.0, 3.0, 4.0, 5.0)
 print(sq1)
 print(cube1)
 print(f"area of sq1: {sq1.area()}")
+print(f"area of c1: {c1.area()}")
 print(f"area of cube1: {cube1.area()}")
+print(f"area of sp1: {sp1.area()}")
+# error: print(f"volume of sq1: {sq1.volume()}")
+print(f"volume of cube1: {cube1.volume()}")
+print(f"volume of sp1: {cube1.volume()}")
+print(f"volume of sp1: {sp1.volume()}")
