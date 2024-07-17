593c53467e5c7c8bbe0ce8d14f34eb813beb01f4
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-17 14:01:57 +0300

    Update exercise03.py

--- module05/exercise03.py
+++ module05/exercise03.py
@@ -7,7 +7,7 @@ document = minidom.parse("resources/countries.xml")
 countries = document.documentElement.getElementsByTagName("country")
 
 
-def find_countries(predicate, sorter):
+def find_countries(predicate, sorter):  # HoF
     result = []
     for country in countries:
         a_country = Country()
@@ -22,8 +22,23 @@ def find_countries(predicate, sorter):
     return result
 
 
-for _country in find_countries(
+def gen_find_countries(predicate, sorter):  # HoF
+    for country in countries:
+        a_country = Country()
+        a_country.code = country.getElementsByTagName("Code")[0].childNodes[0].data
+        a_country.name = country.getElementsByTagName("Name")[0].childNodes[0].data
+        a_country.continent = country.getElementsByTagName("Continent")[0].childNodes[0].data
+        a_country.nufus = country.getElementsByTagName("Population")[0].childNodes[0].data
+        a_country.surfaceArea = float(country.getElementsByTagName("SurfaceArea")[0].childNodes[0].data)
+        if predicate(a_country):
+            print(f"yielding country: {a_country}")
+            yield a_country
+
+
+for _country in gen_find_countries(
         lambda ctry: ctry.continent == "Europe",
         lambda ctry: ctry.nufus
 ):
-    print(_country)
+    print(f"found country: {_country}")
+    if _country.nufus > 10_000_000:
+        break
