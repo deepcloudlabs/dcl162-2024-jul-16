b9e9f31fd48d4dbe4ebbcb1c0696e0c3c18e59b3
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-17 15:51:40 +0300

    initial commit

--- module06/exercise03.py
+++ module06/exercise03.py
@@ -1,17 +1,18 @@
-from threading import Thread
+from threading import Thread, Lock
 
 data: int = 0
-
+lock = Lock()
 
 def task(name: str):
     global data
     print(f"Thread ({name}) is running the task")
     for i in range(0, 1_000_000):
-        local = data
-        local += 1
-        if i % 100_000 == 0:
-            print(f"Thread ({name}): data={data}")
-        data = local
+        with lock:
+            local = data
+            local += 1
+            if i % 100_000 == 0:
+                print(f"Thread ({name}): data={data}")
+            data = local
     print(f"Thread ({name}) is completed: {data}")
 
 
@@ -26,8 +27,7 @@ for t in threads:
     t.join()
 print(f"Application is just completed: {data}")
 """
-Application is just completed: 2888428
-Application is just completed: 3441105
-Application is just completed: 3206465
-Application is just completed: 3030215
+Application is just completed: 9000000
+Application is just completed: 9000000
+
 """
