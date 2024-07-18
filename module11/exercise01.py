76287233dc635ca53db792a9386dd5d91dc5115f
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-18 18:14:23 +0300

    initial commit

---
+++ module11/exercise01.py
@@ -0,0 +1,13 @@
+"""
+Kafka
+1. zookeeper
+   C:\DEVEL\stage\var\scripts\start-zookeeper.bat
+   2024-07-18 16:16:31,822 [myid:] - INFO  [main:NIOServerCnxnFactory@89] - binding to port 0.0.0.0/0.0.0.0:2191
+2. kafka-broker
+   C:\DEVEL\stage\var\scripts\start-kafka-broker2.bat
+   [2024-07-18 16:17:37,145] INFO [KafkaServer id=0] started (kafka.server.KafkaServer)
+3. rabbitmq
+    C:\DEVEL\stage\var\scripts\start-rabbitmq.bat
+    http://localhost:15672
+    demoadmin:secret123
+"""
