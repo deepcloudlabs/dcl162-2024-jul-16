94f13d4c2a19ac52fd1551d585008c2974df52f9
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-18 10:53:49 +0300

    Create exercise05.py

---
+++ module08/exercise05.py
@@ -0,0 +1,19 @@
+from mysql import connector
+
+mysql_connection = connector.connect(
+    host='localhost',
+    user='root',
+    password='Secret_123',
+    database='banking'
+)
+
+cursor = mysql_connection.cursor()
+
+cursor.execute("""
+select iban,balance,status
+from accounts
+where status = 'ACTIVE'
+limit 0,20
+""")
+for iban, balance, status in cursor:
+    print(iban, balance, status)
