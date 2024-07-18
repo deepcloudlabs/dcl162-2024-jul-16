30864e5e6533fa2040ecff03ea45ea6e9d892b7f
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-18 10:50:09 +0300

    Update exercise04.py

--- module08/exercise04.py
+++ module08/exercise04.py
@@ -9,5 +9,15 @@ mysql_connection = connector.connect(
 
 cursor = mysql_connection.cursor()
 accounts = [
-    ("tr1",10_000,'ACTIVE')
+    ("tr1", 10_000, 'ACTIVE'),
+    ("tr2", 20_000, 'ACTIVE'),
+    ("tr3", 30_000, 'CLOSED'),
+    ("tr4", 40_000, 'BLOCKED'),
+    ("tr5", 50_000, 'ACTIVE'),
+    ("tr6", 60_000, 'CLOSED'),
+    ("tr7", 70_000, 'ACTIVE'),
+    ("tr8", 90_000, 'BLOCKED')
 ]
+for iban, balance, status in accounts:
+    cursor.execute(f"insert into accounts values ('{iban}',{balance},'{status}')")
+mysql_connection.commit()
